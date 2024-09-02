37f651f518992e4953eeebb892cef4288b728bd7
atharvagujarathi <75681555+atharvagujarathi@users.noreply.github.com>
2024-09-02 23:16:20 +0530

    02-09-2024

--- data_structures/singlycl.c
+++ data_structures/singlycl.c
@@ -76,6 +76,113 @@ void insertAtLast(PPNODE First, PPNODE Last, int iNo)
 
 void deleteAtFirst(PPNODE First, PPNODE Last)
 {
+    if (*First == NULL && *Last == NULL)
+    {
+        printf("Linked List is empty");
+    }
+    else if (*Last == *First)
+    {
+        free(*First);
+        *First = NULL;
+        *Last = NULL;
+    }
+    else
+    {
+        *First = (*First)->next;
+        free((*Last)->next);
+        (*Last)->next = *First;
+    }
+}
+
+void deleteAtLast(PPNODE First, PPNODE Last)
+{
+    if (*First == NULL && *Last == NULL)
+    {
+        printf("Linked List is empty");
+    }
+    else if (*Last == *First)
+    {
+        free(*First);
+        *First = NULL;
+        *Last = NULL;
+    }
+    else
+    {
+        PNODE temp = *First;
+        while (temp->next != *Last)
+        {
+            temp = temp->next;
+        }
+        free(*Last);
+        *Last = temp;
+        (*Last)->next = *First;
+    }
+}
+
+void insertAtPos(PPNODE First, PPNODE Last, int iPos, int iNo)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = iNo;
+    newn->next = NULL;
+
+    int iLength = 0;
+    iLength = count(*First, *Last);
+
+    if ((iPos < 1) && (iLength + 1 < iPos))
+    {
+        printf("Invalid Position");
+    }
+
+    if (iPos == 1)
+    {
+        insertAtFirst(First, Last, iNo);
+    }
+    else if (iPos == iLength + 1)
+    {
+        insertAtLast(First, Last, iNo);
+    }
+    else
+    {
+        PNODE temp = *First;
+        for (int i = 1; i < iPos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        newn->next = temp->next;
+        temp->next = newn;
+    }
+}
+
+void deleteAtPos(PPNODE First, PPNODE Last, int iPos)
+{
+    int iLength = 0;
+    iLength = count(*First, *Last);
+
+    if ((iPos < 1) && (iLength < iPos))
+    {
+        printf("Invalid Position");
+    }
+
+    if (iPos == 1)
+    {
+        deleteAtFirst(First, Last);
+    }
+    else if (iPos == iLength)
+    {
+        deleteAtLast(First, Last);
+    }
+    else
+    {
+        PNODE temp1 = *First;
+        PNODE temp2 = NULL;
+        for (int i = 1; i < iPos - 1; i++)
+        {
+            temp1 = temp1->next;
+        }
+        temp2 = temp1->next;
+        temp1->next = temp2->next;
+        free(temp2);
+    }
 }
 
 int main()
@@ -95,6 +202,24 @@ int main()
     insertAtLast(&Head, &Tail, 101);
     insertAtLast(&Head, &Tail, 201);
     insertAtLast(&Head, &Tail, 501);
+
+    display(Head, Tail);
+    iRet = count(Head, Tail);
+    printf("The count is %d\n", iRet);
+
+    insertAtPos(&Head, &Tail, 3, 1011);
+
+    display(Head, Tail);
+    iRet = count(Head, Tail);
+    printf("The count is %d\n", iRet);
+
+    // deleteAtPos(&Head, &Tail, 3);
+
+    // display(Head, Tail);
+    // iRet = count(Head, Tail);
+    // printf("The count is %d\n", iRet);
+
+    deleteAtLast(&Head, &Tail);
 
     display(Head, Tail);
     iRet = count(Head, Tail);
